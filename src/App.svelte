b54eae91b35e2b5733ca481a16e2a4426caf4dc9
ga-fleury <gabrielfleury95@gmail.com>
2021-04-26 06:23:12 -0300

    API fetch and SASS working

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,78 @@
 <script>
-  var pokeNumber = Math.floor(Math.random() * 899);
-  var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + pokeNumber;
-  let currentPokemon = {};
-  let img;
+    var pokeNumber = Math.floor(Math.random() * 899);
+    var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + pokeNumber;
+    let currentPokemon = {};
+    let pokeImage;
+    let pokeTypes = [];
 
-  fetch(pokeURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then((json) => {
-      currentPokemon = json;
-      img = currentPokemon.sprites.other["official-artwork"].front_default;
-      console.log(currentPokemon);
-    });
+    // POKE API FETCH
+
+    // fetch(pokeURL)
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then((json) => {
+    //     currentPokemon = json;
+    //     pokeImage = currentPokemon.sprites.other["official-artwork"].front_default;
+    //     console.log(currentPokemon);
+    //   });
+
+    fetch("bulba.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then((json) => {
+            currentPokemon = json;
+            pokeImage = currentPokemon.sprites.other["official-artwork"].front_default;
+            pokeTypes = Object.values(currentPokemon.types);
+
+            console.log(currentPokemon);
+        });
 </script>
 
 <main>
-  <h1>{currentPokemon.name} #{currentPokemon.id}</h1>
-  <div class="pokewrapper">
-    <img src={img} alt="" />
-  </div>
+    <h1>{currentPokemon.name} #{currentPokemon.id}</h1>
+    <div class="pokewrapper">
+        <img src={pokeImage} alt="" />
+        {#each pokeTypes as type}
+            <img src="./img/{type.type.name}.png" alt="" class="poketype" />
+            <p class="background-color-{type.type.name}">{type.type.name}</p>
+        {/each}
+    </div>
 </main>
 
-<style>
-  .pokewrapper {
-    display: flex;
-    justify-content: center;
-  }
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
+<style lang="scss">
+    $color-grass: #9bcc50;
+    $color-poison: #9bcc50;
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .background-color-grass {
+    background-color: $color-grass;
   }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+    .poketype {
+        width: 100px;
     }
-  }
+    .pokewrapper {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+    }
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
